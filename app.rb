c8e3977d6de678b714e237adc77ef17d815b4330
Taylor Williams <taylor@thewilliams.net>
2018-08-12 01:50:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,28 +6,37 @@ class App < Sinatra::Base
     @name = params[:name]
     "#{@name.reverse}"
   end
-  
+
   get '/square/:number' do
     @num = params[:number]
     "#{@num.to_i * @num.to_i}"
   end
-  
+
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
     @num = params[:number]
+    @num.to_i times do
+      "#{@phrase}"
+    end
   end
-  
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @one = params[:word1]
     @two = params[:word2]
     @three = params[:word3]
     @four = params[:word4]
     @five = params[:word5]
+    "#{one} #{two} #{three} #{four} #{five}"
   end
-  
+
   get '/:operation/:number1/:number2' do
-    @one = params[:number1]
-    @two = params[:number2]
+    @operation = params[:operation]
+    @one = params[:number1].to_i
+    @two = params[:number2].to_i
+    case @operation
+    when "add"
+      
+    end
   end
-  
+
 end
